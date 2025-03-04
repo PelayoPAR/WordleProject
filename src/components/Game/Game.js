994ab11d2c5cd061e57420378d110ba132d0ce4e
PelayoPAR <pelayopar@gmail.com>
2025-03-04 17:25:39 +0100

    cleanup status logs from UI

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,38 +1,48 @@
-import React from 'react';
+import React from "react"
 
-import { sample } from '../../utils';
-import { WORDS } from '../../data';
-import GuessInput from '../GuessInput';
-import GuessTable from '../GuessTable';
-import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
-import GameOverBanner from '../GameOverBanner'
+import { sample } from "../../utils"
+import { WORDS } from "../../data"
+import GuessInput from "../GuessInput"
+import GuessTable from "../GuessTable"
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants"
+import GameOverBanner from "../GameOverBanner"
 
-// Pick a random word on every pageload. 
-const answer = sample(WORDS);
+// Pick a random word on every pageload.
+const answer = sample(WORDS)
 // To make debugging easier, we'll log the solution in the console.
-console.info({ answer });
+console.info({ answer })
 
 function Game() {
-const [guesses, setGuesses] = React.useState([])
-const [gameStatus, setGameStatus] = React.useState("running")
+  const [guesses, setGuesses] = React.useState([])
+  const [gameStatus, setGameStatus] = React.useState("running")
 
-function handleSubmitGuess(tentativeGuess) {
-  const nextGuesses = [...guesses, tentativeGuess] // trigger rerender to update guesses.length instead of using NUM_OF_GUESSES_ALLOWED - 1 in order to lose the game on last try + 1
-  setGuesses(nextGuesses);
+  function handleSubmitGuess(tentativeGuess) {
+    const nextGuesses = [...guesses, tentativeGuess] // trigger rerender to update guesses.length instead of using NUM_OF_GUESSES_ALLOWED - 1 in order to lose the game on last try + 1
+    setGuesses(nextGuesses)
 
-  if (tentativeGuess.toUpperCase() === answer) {
-    setGameStatus("won")
-  } else if (nextGuesses.length >= NUM_OF_GUESSES_ALLOWED)  {
-    setGameStatus("lost")
+    if (tentativeGuess.toUpperCase() === answer) {
+      setGameStatus("won")
+    } else if (nextGuesses.length >= NUM_OF_GUESSES_ALLOWED) {
+      setGameStatus("lost")
+    }
   }
-}
 
-  return <>
-      {gameStatus}
+  return (
+    <>
       <GuessTable guesses={guesses} answer={answer}></GuessTable>
-      <GuessInput handleSubmitGuess={handleSubmitGuess} gameStatus={gameStatus}></GuessInput>
-      {gameStatus !== 'running' && (<GameOverBanner gameStatus={gameStatus} numOfGuesses={guesses.length} answer={answer}/>)}
-    </>;
+      <GuessInput
+        handleSubmitGuess={handleSubmitGuess}
+        gameStatus={gameStatus}
+      ></GuessInput>
+      {gameStatus !== "running" && (
+        <GameOverBanner
+          gameStatus={gameStatus}
+          numOfGuesses={guesses.length}
+          answer={answer}
+        />
+      )}
+    </>
+  )
 }
 
-export default Game;
+export default Game
