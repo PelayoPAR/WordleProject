9ea8b2dfa00c2776100383616d6ca06c0ad6d83d
PelayoPAR <pelayopar@gmail.com>
2025-03-11 15:00:16 +0100

    do trial

--- src/components/GuessTable/GuessTable.js
+++ src/components/GuessTable/GuessTable.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react"
 import { NUM_OF_GUESSES_ALLOWED } from "../../constants"
-import GuessRow from './GuessRow';
-import { range } from '../../utils';
+import GuessRow from "./GuessRow"
+import { range } from "../../utils"
 
-function GuessTable({guesses, answer}) {
-
+function GuessTable({ guesses, answer }) {
   return (
-  <div className="guess-results">
-    {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
-      <GuessRow key={num} value={guesses[num]} answer={answer}/>
-    ))}
- </div> );
+    <div className="guess-results">
+      {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
+        <GuessRow key={num} value={guesses[num]} answer={answer} />
+      ))}
+    </div>
+  )
 }
 
-export default GuessTable;
+export default GuessTable
